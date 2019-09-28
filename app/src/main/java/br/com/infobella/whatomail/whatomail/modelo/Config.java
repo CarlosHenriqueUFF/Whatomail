3e341711f6d2618873daa622cf53a00e99cb837e
Carlos Henrique <henriquezplay@hotmail.com>
2019-09-27 21:33:47 -0300

    File config restore

--- app/src/main/java/br/com/infobella/whatomail/whatomail/modelo/Config.java
+++ app/src/main/java/br/com/infobella/whatomail/whatomail/modelo/Config.java
@@ -12,6 +12,22 @@ public class Config {
     public static final String VALUE_SENDER = "Whatomail";
     public static final String VALUE_SUBJECT = "Whatomail: Conversas Recebidas via WhatsApp";
     //Dados da Conta de Email que faz o envio
+    //************* Niterói
+    //public static final String VALUE_EMAIL_FROM = "drogavetniteroi2@gmail.com";
+    //public static final String VALUE_PASSWORD = "dv280213";
+    //************* Vitória
+    public static final String VALUE_EMAIL_FROM = "drogavetvitoria@gmail.com";
+    public static final String VALUE_PASSWORD = "Drogavetvt2547*";
+    //*********** João Pessoa
+    //public static final String VALUE_EMAIL_FROM = "receitaswhatsapp@gmail.com";
+    //public static final String VALUE_PASSWORD = "999999999";
+    public static final String VALUE_SERVER_PORT = "587";
+    public static final String VALUE_SERVER_SMTP = "smtp.gmail.com";
+    //Destinatários
+    public static final String VALUE_EMAIL_COPY = "whatomaildrogavetniteroi@gmail.com";
+    public static final String VALUE_EMAIL_TO = "drogavetniteroi@drogavet.com";
+    //public static final String VALUE_EMAIL_COPY = "whatomaildrogavetniteroi@gmail.com";
+    //public static final String VALUE_EMAIL_TO = "drogavetniteroi@drogavet.com";
     //Valores Padrões de configuração
     public static final String VALUE_TRUE = "true";
     public static final String VALUE_FALSE = "false";
@@ -24,5 +40,12 @@ public class Config {
     public static final String PROPS_MAIL_SMTP_SSL_TRUST = "mail.smtp.ssl.trust";
     //Configuração de Acesso FreshDesk
     public static final boolean SEND_FRESHDESK = true;
+    //public static final String apiToken = "deJFCMfs4Zy6SdzlpNJl";
+    //public static final String apiEndpoint = "http://drogavetniteroi.freshdesk.com";
 
+    public static final String apiToken = "EhWLwrQNpSJd1UWSVtW8";
+    public static final String apiEndpoint = "http://drogavetvitoria.freshdesk.com";
+
+    //public static final String apiToken = "Fao7blZR6HFH5sTfU3Xi";
+    //public static final String apiEndpoint = "http://drogavetjoaopessoa.freshdesk.com";
 }
